210853acbab6e47b0eea78d130f3d3080f751dd5
Stephan Lagraulet <slagraulet@ippon.fr>
2016-04-12 22:59:59 +0200

    Small changes

--- searcher-service/src/main/java/fr/ippon/microservices/controller/SearchController.java
+++ searcher-service/src/main/java/fr/ippon/microservices/controller/SearchController.java
@@ -1,8 +1,10 @@
 package fr.ippon.microservices.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.ippon.microservices.config.Config;
-import fr.ippon.microservices.model.CityResult;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
@@ -14,52 +16,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.ippon.microservices.model.CityResult;
 
 @RestController
 @RequestMapping("/search")
 public class SearchController {
 
-    @Inject
-    private Config config;
+	@Inject
+	private Client esClient;
 
-    @Inject
-    private Client esClient;
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+	@RequestMapping(method = RequestMethod.GET, value = "/city/{city}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<CityResult> searchByCity(@PathVariable String city) throws Exception {
+		SearchResponse response = esClient.prepareSearch("aoc_aop").setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+				.setQuery(QueryBuilders.matchQuery("commune", city)).execute().actionGet();
 
-    @RequestMapping(method = RequestMethod.GET, value = "/city/{city}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CityResult> searchByCity(@PathVariable String city) throws Exception {
-        SearchResponse response = esClient.prepareSearch("aoc_aop")
-                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(QueryBuilders.matchQuery("commune", city))
-                .execute()
-                .actionGet();
+		List<CityResult> cityResults = new ArrayList<>();
+		if (response != null) {
+			for (SearchHit hit : response.getHits().getHits()) {
+				CityResult cityResult = objectMapper.readValue(hit.getSourceAsString(), CityResult.class);
+				cityResults.add(cityResult);
+			}
+		}
+		return cityResults;
+	}
 
-        List<CityResult> cityResults = new ArrayList<>();
-        if(response != null) {
-            for(SearchHit hit : response.getHits().getHits()) {
-                CityResult cityResult = objectMapper.readValue(hit.getSourceAsString(), CityResult.class);
-                cityResults.add(cityResult);
-            }
-        }
-        return cityResults;
-    }
+	@RequestMapping(method = RequestMethod.GET, value = "/geo/{aireGeo}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> searchByAireGeo(@PathVariable String aireGeo) throws Exception {
+		SearchResponse response = esClient.prepareSearch("aoc_aop").setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+				.setQuery(QueryBuilders.matchPhraseQuery("aire_geo", aireGeo)).execute().actionGet();
 
-    @RequestMapping(method = RequestMethod.GET, value = "/geo/{aireGeo}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<String> searchByAireGeo(@PathVariable String aireGeo) throws Exception {
-        SearchResponse response = esClient.prepareSearch("aoc_aop")
-                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-                .setQuery(QueryBuilders.matchPhraseQuery("aire_geo", aireGeo))
-                .execute()
-                .actionGet();
-
-        List<String> communes = new ArrayList<String>();
-        for(SearchHit hit : response.getHits().getHits()) {
-            communes.add(hit.getSource().get("commune") + " - (" + hit.getSource().get("departement") + ")");
-        }
-        return communes;
-    }
+		List<String> communes = new ArrayList<String>();
+		for (SearchHit hit : response.getHits().getHits()) {
+			communes.add(hit.getSource().get("commune") + " - (" + hit.getSource().get("departement") + ")");
+		}
+		return communes;
+	}
 }
